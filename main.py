b987e9b795c72cffcf76090051342d5dc6fe3bd9
Yaa-K <yaa.k.yeboah@outlook.com>
2026-02-12 08:04:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import random
 
 app = FastAPI()
 
+class LearnerData(BaseModel):
+    attendance: int
+    average_score: float
+    login_frequency: int
+
 @app.get("/")
 def root():
-    return {"status": "success", "message": "CI/CD works!"}
+    return {"status": "Backend running"}
+
+@app.post("/predict")
+def predict(data: LearnerData):
+    # Fake AI logic (simulate risk score)
+    risk_score = random.uniform(0, 1)
+
+    if risk_score > 0.7:
+        risk_level = "High Risk"
+    elif risk_score > 0.4:
+        risk_level = "Medium Risk"
+    else:
+        risk_level = "Low Risk"
+
+    return {
+        "risk_score": round(risk_score, 2),
+        "risk_level": risk_level,
+        "explanation": "Risk determined based on engagement patterns."
+    }
